f4f845ff71ca4292590c476f917cd4e8df6b4caf
Katleiah <katleiahramos@gmail.com>
2018-03-08 17:41:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,9 +13,8 @@ end
 def long_planeteer_calls(array)
   array.collect{|x|
      if x.length > 4?  true : false 
-
     }
-  # Your code here
+  end 
 end
 
 def find_the_cheese# code an argument here

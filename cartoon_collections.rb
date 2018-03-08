ad823fd1b1d36e63489f9327a51f1af01635f188
Katleiah <katleiahramos@gmail.com>
2018-03-08 17:46:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,9 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.collect {|x| if x.length > 4? true : false }}
-
+  array.each do |x|
+    if x.length > 4? true : false 
+  
 end
 
 def find_the_cheese# code an argument here

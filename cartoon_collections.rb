a558d7935050ad8435a39658bafb7368ef70b777
Katleiah <katleiahramos@gmail.com>
2018-03-08 16:53:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
 def roll_call_dwarves(array)
   # Your code here
-  array.each_with_index {|item, index| 
+  array.each_with_index {|item, index|
     puts "#{index}. #{item}"
-  } 
+  }
 end
 
 end

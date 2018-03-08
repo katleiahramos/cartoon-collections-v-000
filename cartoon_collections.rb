ca7bfed22ef398130addd6d288044d40f3aa2372
Katleiah <katleiahramos@gmail.com>
2018-03-08 16:56:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,12 +2,11 @@ require 'pry'
 
 def roll_call_dwarves(array)
   # Your code here
-  binding.pry
+
   array.each_with_index {|item, index|
+    binding.pry
     puts "#{index}. #{item}"
   }
-
-
 end
 
 
